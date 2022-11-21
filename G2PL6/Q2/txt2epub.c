19f9b4bdde08a0504df8b00b3acb9002251f97af
Francisco Ana <franciscopdana@gmail.com>
2022-11-21 18:35:07 +0000

    ex2 almost-done: falta ainda remover warnings

--- G2PL6/Q2/txt2epub.c
+++ G2PL6/Q2/txt2epub.c
@@ -9,11 +9,16 @@
 // gcc -Wall txt2epub.c -o txt2epub
 
 int main (int argc, char* argv[]){
+    // Example:
+    // argv = { "txt2epub", "file1.txt", "file1.txt" } -> argc = 3
+    // zip_arguments = { "zip", "ebooks.epub", "file1.epub", "file1.txt", NULL }
+
     pid_t pid;
-    char* zip_arguments[argc+1];
+    char* zip_arguments[argc+2];
     zip_arguments[0] = "zip";
     zip_arguments[1] = "ebooks.zip";
-    
+    zip_arguments[argc+1] = NULL;
+
     for (int i = 1; i < argc; i++){ 
         // argv[i] is the .txt file we want to convert
         // i want a string with the name of the .epub file
@@ -24,13 +29,11 @@ int main (int argc, char* argv[]){
 
         zip_arguments[i+1] = epub_name;
 
-        //TODO: enchament: set metadata correctly
-
-        // create command: pandoc argv[i](.txt) -o epub_name(.epub) 
         char* txt_name = malloc(strlen(argv[i]) + 1);
         strcpy(txt_name, argv[i]);
 
-        char* arguments[1024] = {"pandoc", txt_name, "-o", epub_name, NULL };
+        // TODO: create command: pandoc txt_name(.txt) -o epub_name(.epub)
+        char* arguments[] = { "pandoc", txt_name, "-o", epub_name, NULL };
 
         if((pid = fork()) == - 1){
             fprintf(stderr, "pandoc: can't fork command: %s\n", strerror(errno));
@@ -41,14 +44,27 @@ int main (int argc, char* argv[]){
             // execute command
             execvp(arguments[0], arguments);
             fprintf(stderr, "pandoc: can't execute command: %s\n", strerror(errno));
-            continue;
+            continue; 
         }
         else{
             wait(NULL);
         }
     }
     
-    // TODO: zip all the .epub files
+    if((pid = fork()) == - 1){
+        fprintf(stderr, "zip: can't fork command: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    else if(pid == 0){
+        printf("[pid%d] creating ebooks.zip..\n", getpid()); 
+        // execute command
+        execvp(zip_arguments[0], zip_arguments);
+        fprintf(stderr, "zip: can't execute command: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    else{
+        wait(NULL);
+    }
 
     return EXIT_SUCCESS;
 }
