d34c870341e1654d000440d1c65d0fb4a8165aed
Francisco Ana <franciscopdana@gmail.com>
2022-11-28 18:47:38 +0000

    correct some mistakes

--- G2PL6/Q2/txt2epub.c
+++ G2PL6/Q2/txt2epub.c
@@ -3,16 +3,9 @@
 #include <errno.h>
 #include <unistd.h>
 #include <sys/wait.h>
-
 #include <string.h>
 
-// gcc -Wall txt2epub.c -o txt2epub
-
 int main (int argc, char* argv[]){
-    // Example:
-    // argv = { "txt2epub", "file1.txt", "file1.txt" } -> argc = 3
-    // zip_arguments = { "zip", "ebooks.epub", "file1.epub", "file1.txt", NULL }
-
     pid_t pid;
 
     char* zip_arguments[argc+2];
@@ -35,6 +28,7 @@ int main (int argc, char* argv[]){
 
         // add file.epub to zip command
         zip_arguments[i+1] = epub_name;
+
 
         char* txt_name = malloc(strlen(argv[i]) + 1);
         strcpy(txt_name, argv[i]);
