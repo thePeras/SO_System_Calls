76aef03ef08e5aaca451c2a45344e80629c14533
thePeras <jtopereira@gmail.com>
2022-11-28 23:51:22 +0000

    finished q3

--- G2PL6/Q3/tokenring.c
+++ G2PL6/Q3/tokenring.c
@@ -6,122 +6,156 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <string.h>
+#include <signal.h>
 
-#define NUMBERSTRINGSIZE 12
+// Custom mod function
+static int mod(int a, int b){
+    int r = a % b;
+    return r < 0 ? r + b : r;
+}
+
+// Global variables
+pid_t parentPID;
+int numberProc;
+
+// Process variables
+int process_pipe_in;
+int process_pipe_out;
+char* process_pipe_name;
+
+// SIGINT Signal handler
+static void close_pipes(){
+    if (getpid() == parentPID) {
+        for (int i = 0; i < numberProc; i++) wait(NULL);
+        printf("\n");
+    }
+    else {
+        // Closing pipes
+        if (close(process_pipe_in) < 0) {
+            fprintf(stderr, "Erro a fechar pipe de input. %s\n", strerror(errno));
+        }
+        if (close(process_pipe_out) < 0) {
+            fprintf(stderr, "Erro a fechar pipe de output. %s\n", strerror(errno));
+        }
+        // Unlinking pipe
+        if (unlink(process_pipe_name) < 0) {
+            fprintf(stderr, "Erro a dar unlink ao pipe. %s\n", strerror(errno));
+        }
+    }
+
+    exit(EXIT_SUCCESS);
+}
 
 int main(int argc, char** argv) {
+    parentPID = getpid();
+    int token = 0;
+
+    // Overwriting SIGINT handler
+    if(signal(SIGINT, close_pipes) == SIG_ERR) {
+        fprintf(stderr, "Não consegue apanhar SIGINT: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // Checking arguments
     if(argc < 4) {
-        fprintf(stderr, "faltam argumentos \nUsage: %s <number_processes> <probability_to_sleep> <time_to_sleep> \n", argv[0]);
+        fprintf(stderr, "Faltam argumentos \nUsage: %s <number_processes> <probability_to_sleep> <time_to_sleep> \n", argv[0]);
         return EXIT_FAILURE;
     }
     if(argc > 4) {
-        fprintf(stderr, "argumentos a mais \nUsage: %s <number_processes> <probability_to_sleep> <time_to_sleep> \n", argv[0]);
+        fprintf(stderr, "Argumentos a mais \nUsage: %s <number_processes> <probability_to_sleep> <time_to_sleep> \n", argv[0]);
         return EXIT_FAILURE;
     }
-    int numberProc = atoi(argv[1]);
+    numberProc = atoi(argv[1]);
     float probability = atof(argv[2]);
     int time = atoi(argv[3]);
-    if (numberProc == 0) {
-        fprintf(stderr, "numero de processos invalido \n");
+
+    if (numberProc <= 1) {
+        fprintf(stderr, "Número de processos inválido.\n");
         return EXIT_FAILURE;
     }
-    if (probability == 0) {
-        fprintf(stderr, "probabilidade invalida \n");
+    if (probability <= 0 || probability > 1) {
+        fprintf(stderr, "Probabilidade inválida. Introduza uma probabilidade entre: ]0, 1] \n");
         return EXIT_FAILURE;
     }
     int probability_int = (int)(probability * 100);
-    if (time == 0) {
-        fprintf(stderr, "tempo invalido \n");
+    if (time <= 0) {
+        fprintf(stderr, "Tempo inválido \n");
+        return EXIT_FAILURE;
     }
 
-    // TODO: numberProc < 1000
-
-    // create the pipes
+    // Create pipes
+    char * pipes[numberProc];
     for (int i = 1; i <= numberProc; i++) {
-        char pipeName[12]; 
-        sprintf(pipeName, "pipe%dto%d", i, (i % numberProc) + 1);
+        char pipeName[12];
+        sprintf(pipeName, "pipe%dto%d", i, mod(i, numberProc)+ 1);
+        //copy the pipe name to the array
+        pipes[i-1] = malloc(strlen(pipeName) + 1);
+        strcpy(pipes[i-1], pipeName);
         if (mkfifo(pipeName, 0666) == -1) {
-            fprintf(stderr, "erro ao criar pipe %s \n", pipeName);
+            fprintf(stderr, "Erro ao criar pipe %s: %s \n", pipeName, strerror(errno));
             return EXIT_FAILURE;
         }
     }
-    int token = 0;
-    int pipe = open("pipe1to2", O_WRONLY | O_NONBLOCK);
-    if (pipe < 0) {
-        fprintf(stderr, "erro ao abrir pipe pipe1to2 \n");
-        return EXIT_FAILURE;
-    }
-    write(pipe, &token, sizeof(token));
-    // create the processes
-
-    for (int i = 1; i < numberProc; i++) { //i=2
-        printf("66\n");
+    
+    // Creating processes
+    for (int i = 0; i < numberProc; i++) { 
         pid_t pid = fork();
         if (pid == -1) {
-            fprintf(stderr, "erro ao criar processo %d \n", i);
+            fprintf(stderr, "Erro ao criar processo %d: %s. \n", i + 1, strerror(errno));
             return EXIT_FAILURE;
         }
         if (pid == 0) {
-            // child process 3
-            // open the pipes 2->3
-            char pipeName[12];
-            sprintf(pipeName, "pipe%dto%d", i, (i % numberProc) + 1);   // pipe to read
-            int pipe = open(pipeName, O_RDONLY);
-            if (pipe < 0) {
-                fprintf(stderr, "erro ao abrir pipe %s \n", pipeName);
-                return EXIT_FAILURE;
-            }
-            char pipeName2[12];
-            sprintf(pipeName2, "pipe%dto%d", i, (i + 1) % numberProc);  // pipe to write
-            int pipe2 = open(pipeName2, O_WRONLY);
-            if (pipe2 < 0) {
-                fprintf(stderr, "erro ao abrir pipe %s \n", pipeName2);
-                return EXIT_FAILURE;
-            }
+            // Child process 
+            srandom(getpid());
 
-            int number, nbytes;
-            char stringnumber[NUMBERSTRINGSIZE];
-            while((nbytes = read(pipe, stringnumber, NUMBERSTRINGSIZE)) > 0) {
-                number = atoi(stringnumber);
-                printf("Processo %d recebeu o numero %d \n", i, number);
-                number++;
-                sprintf(stringnumber, "%d", number);
-                // write(pipe2, &number, sizeof(int));
-                write(pipe2, stringnumber, NUMBERSTRINGSIZE);
-                printf("Processo %d enviou o numero %d \n", i, number);
-                if (number % probability_int == 0) {
-                    sleep(time);
+            if(i == 0){
+                process_pipe_out = open(pipes[0], O_WRONLY); // pipe to write
+                if (process_pipe_out < 0) {
+                    fprintf(stderr, "Erro ao abrir pipe %s: %s \n", pipes[0], strerror(errno));
+                    return EXIT_FAILURE;
+                }
+                write(process_pipe_out, (char*) &token, sizeof(int));
+            }
+            char* pipeName = pipes[mod(i-1, numberProc)];
+            process_pipe_name = pipeName;
+            process_pipe_in = open(pipeName, O_RDONLY); // pipe to read
+            if (process_pipe_in < 0) {
+                fprintf(stderr, "Erro ao abrir para ler pipe %s: %s \n", pipeName, strerror(errno));
+                return EXIT_FAILURE;
+            }
+            if(i != 0){
+                char* pipeName2 = pipes[i];  // pipe to write
+                process_pipe_out = open(pipeName2, O_WRONLY);
+                if (process_pipe_out < 0) {
+                    fprintf(stderr, "Erro ao abrir para escrever pipe %s: %s \n", pipeName2, strerror(errno));
+                    return EXIT_FAILURE;
                 }
             }
             
-            float rand = random() % 100;  // 0 - 99
-            if (rand < probability_int) {
-                printf("[p%d] lock on token (val = %d)\n", i, number);
-                sleep(time);
-                printf("[p%d] unlock token\n", i);
-            }
+            free(pipes[i]);
             
-            if (write(pipe2, stringnumber, strlen(stringnumber)) < 0) {
-                fprintf(stderr, "Unable to write to pipe: %s\n", strerror(errno));
-                exit(EXIT_FAILURE);
+            int nbytes = 0;
+            while((nbytes = read(process_pipe_in, (char*) &token, sizeof(int))) > 0) {
+                token++;
+                int rand = mod(random(), 100);  
+                if (rand < probability_int) {
+                    printf("[p%d] lock on token (val = %d)\n", i+1, token);
+                    sleep(time);
+                    printf("[p%d] unlock token \n", i+1);
+                }
+
+                if (write(process_pipe_out, (char*)&token, sizeof(int)) < 0) {
+                    fprintf(stderr, "Erro ao escrever para o pipe: %s. \n", strerror(errno));
+                    exit(EXIT_FAILURE);
+                }
             }
-            close(pipe);
-            close(pipe2);
-        } 
+    
+            fprintf(stderr, "Erro ao ler do pipe: %s. \n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
     }
-
-    // wait for all the processes to finish
-    for (int i = 1; i < numberProc; i++) {
-        wait(NULL);
-    }
-
-    // delete the pipes
-
-
-    // 1- Criar pipes e conectar
-    // 2- Criar processos
-    // 3- Enviar token
-        // 2.1- Calcular probabilidade
-
-    return EXIT_SUCCESS;
+    
+    pause();
+    fprintf(stderr, "Erro ao receber/processar sinal: %s \n", strerror(errno));
+    return EXIT_FAILURE;
 }
