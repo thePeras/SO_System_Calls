827a1f6960e727c6ace446fa288018546815375a
thePeras <jtopereira@gmail.com>
2023-01-10 12:40:20 +0000

    q2 modified´

--- Exercise 2 - Pandoc/txt2epub.c
+++ Exercise 2 - Pandoc/txt2epub.c
@@ -11,59 +11,55 @@ int main (int argc, char* argv[]){
         fprintf(stderr, "Faltam argumentos\nUsage: %s <file1.txt> [... file_n.txt] \n", argv[0]);
         return EXIT_FAILURE;
     }
-    char* zip_arguments[argc+2];
-    zip_arguments[0] = "zip";
-    zip_arguments[1] = "ebooks.zip";
-    zip_arguments[argc+1] = NULL;
+    
+    char **zip_arguments = (char **)malloc((argc + 2) * sizeof(char *));
+    zip_arguments[0] = (char *)malloc(3 * sizeof(char));
+    strcpy(zip_arguments[0], "zip");
+    zip_arguments[1] = (char *)malloc(10 * sizeof(char));
+    strcpy(zip_arguments[1], "ebooks.zip");
+
+    for(int i = 1; i<argc; i++){
+        zip_arguments[i+1] = malloc(strlen(argv[i]) + 1);
+        stpncpy(zip_arguments[i+1], argv[i], strlen(argv[i]) - 4);
+        strcat(zip_arguments[i+1], ".epub");
+    }
 
     for (int i = 1; i < argc; i++){ 
-        char* epub_name = malloc(strlen(argv[i]) + 1);
-        strcpy(epub_name, argv[i]);
-        epub_name[strlen(argv[i]) - 4] = '\0';
-        strcat(epub_name, ".epub");
-
-        char* title = malloc(strlen(argv[i]) + 7);
-        strcpy(title, "title=");
-        strcat(title, argv[i]);
-        title[strlen(argv[i]) + 6] = '\0';
-
-        zip_arguments[i+1] = epub_name;
-
-
-        char* txt_name = malloc(strlen(argv[i]) + 1);
-        strcpy(txt_name, argv[i]);
-
-        char* pandoc_arguments[] = { "pandoc", txt_name, "-o", epub_name, "--metadata", title, NULL };
-
         if((pid = fork()) == - 1){
             fprintf(stderr, "pandoc: Não conseguiu dar fork: %s\n", strerror(errno));
             continue;
         }
         else if(pid == 0){
+            char* title = malloc(strlen(argv[i]) + 7);
+            strcpy(title, "title=");
+            strncat(title, argv[i], strlen(argv[i]) - 4);
+            title[strlen(argv[i]) + 6] = '\0';
+
+            char* txt_name = malloc(strlen(argv[i]) - 1);
+            stpncpy(txt_name, argv[i], strlen(argv[i]) - 4);
+            strcat(txt_name, ".txt");
+
+            char* pandoc_arguments[] = { "pandoc", txt_name, "-o", zip_arguments[i+1], "--metadata", title, NULL };
+
             printf("[pid%d] converting %s..\n", getpid(), txt_name); 
             execvp(pandoc_arguments[0], pandoc_arguments);
             fprintf(stderr, "pandoc: Comando não executado: %s\n", strerror(errno));
             continue; 
         }
-        else{
-            wait(NULL);
-        }
-    }
-    
-    if((pid = fork()) == - 1){
-        fprintf(stderr, "zip: Não conseguiu dar fork: %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-    else if(pid == 0){
-        printf("[pid%d] a criar ebooks.zip..\n", getpid()); 
-        execvp(zip_arguments[0], zip_arguments);
-        fprintf(stderr, "zip: Comando não executado: %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-    else{
-        wait(NULL);
     }
 
-    return EXIT_SUCCESS;
+    for(int i = 0; i < argc; i++){
+        wait(NULL);
+    }
+    
+    printf("A criar ebooks.zip..\n"); 
+    for (int i = 0; i < argc+1; i++){
+        printf("%s ", zip_arguments[i]);
+    }
+    execvp(zip_arguments[0], zip_arguments);
+
+    // This only runs if execvp fails
+    fprintf(stderr, "zip: Comando não executado: %s\n", strerror(errno));
+    return EXIT_FAILURE;
 }
 
